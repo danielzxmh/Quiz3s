ee1eed8a09a9dc7a6b3fc6fe9e3c42a8365f1c1d
daniel <daniel.mosquerahe@amigo.edu.co>
2024-11-20 19:08:27 -0500

    lo que quieras

---
+++ Js/Script.js
@@ -0,0 +1,33 @@
+var inputTarea = document.getElementById("tarea");
+var btn = document.getElementById("agregar");
+var listado = document.getElementById("listado");
+var cantidad = document.getElementById("cantidad");
+
+var total = 0;
+
+btn.onclick = function() {
+    if (inputTarea.value == "") {
+        return;
+    }
+    var elemento = inputTarea.value;
+    var li = document.createElement("li");
+    li.textContent = elemento;
+    listado.appendChild(li);
+    total++;
+    cantidad.innerHTML = total;
+
+
+    var btnEliminar = document.createElement("span");
+    btnEliminar.textContent = "\u00d7";
+    li.appendChild(btnEliminar);
+
+
+    btnEliminar.onclick = function() {
+        li.remove();
+        total--;
+        cantidad.innerHTML = total;
+    }
+
+    inputTarea.value = "";
+
+}
